8bafaec834da09d50a40c2525d4d574b4b38d2cf
poolr-ridesharing <84660656+poolr-ridesharing@users.noreply.github.com>
2023-04-14 13:30:28 +0300

    added more components and sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import Navbar from '@/components/Navbar'
 import Products from '@/components/ProductImage'
 import Hero from '@/components/Hero'
 import MostDemandedProducts from '@/components/MostPopularProducts'
+import Footer from '@/components/Footer'
+import SubscriptionPrompt from '@/components/SubscriptionPrompt'
+import Testimonials from '@/components/Testimonials'
+import OurTeam from '@/components/OurTeam'
+import Header from '@/components/Header'
 
 
 
@@ -11,7 +16,15 @@ export default function Home() {
     <>
 <Navbar/>
 <Hero />
+
 <MostDemandedProducts/>
+<Header/>
+<Testimonials/>
+
+<OurTeam/>
+
+<SubscriptionPrompt/>
+<Footer/>
     </>
   )
 }
